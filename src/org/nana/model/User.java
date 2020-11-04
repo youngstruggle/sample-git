c1767f6ac7361f6c16b27bf26ce3d76bfaabb8e9
youngstruggle <febrian.nana@gmail.com>
2020-11-04 21:47:39 +0700

    added getter and setter

--- src/org/nana/model/User.java
+++ src/org/nana/model/User.java
@@ -3,4 +3,13 @@ package org.nana.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
